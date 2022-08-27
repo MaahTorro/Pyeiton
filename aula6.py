9146613e16ec24e6a99d9cd1a5362f1da0b1473d
Marcela Torro <marcelatorro6@gmail.com>
2022-08-27 00:15:55 -0300

    de 25 pra 20 :)

--- aula6.py
+++ aula6.py
@@ -1,6 +1,6 @@
 numero = 0 
 
-de0a25 = 0
+de0a20 = 0
 de26a50 = 0
 de51a75 = 0 
 de76a100 = 0
@@ -9,8 +9,8 @@ while numero >= 0:
     numero = int(input("Digite um valor: "))
 
     if numero >= 0:
-        if numero <= 25:
-            de0a25 += 1
+        if numero <= 20:
+            de0a20 += 1
         else:
             if numero <=50:
                 de26a50 += 1
@@ -21,7 +21,7 @@ while numero >= 0:
                     if numero <= 100:
                         de76a100 += 1
 
-print(de0a25)
+print(de0a20)
 print(de26a50)
 print(de51a75)
 print(de76a100)
